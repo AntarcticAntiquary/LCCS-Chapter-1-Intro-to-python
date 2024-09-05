c39a08ad9e9e007c77b5375ea6a954c62e6fd695
AntarcticAntiquary <ctw20.daniel.lewis@corketb.ie>
2024-09-05 22:09:33 +0100

    Add files via upload

---
+++ Page 12 Task 4.py
@@ -0,0 +1,11 @@
+"""
+Author = 'Daniel Lewis'
+Telos = Homework 5/9/24
+Identifier = pg 12 Task 4
+"""
+
+print("x  x  xx  xx   xx xxxx    xxxx  xxx")
+print("x  x x  x x x x x x       x    x")
+print("xxxx x  x x  x  x xxx     xxx  x")
+print("x  x x  x x     x x       x    x")
+print("x  x  xx  x     x xxxx    xxxx  xxx")
