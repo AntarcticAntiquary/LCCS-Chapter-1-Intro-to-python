f5dd1605f33903cf5ffeca8f3d85703dd511c539
AntarcticAntiquary <ctw20.daniel.lewis@corketb.ie>
2024-09-06 14:18:20 +0100

    Add files via upload

---
+++ Classwork_Variables.py
@@ -0,0 +1,38 @@
+# Author = 'Daniel Lewis'
+# Telos = 'Classwork'
+# Topic = 'Variables & Arithmetic Opeerations'
+# Identifier = [null]
+# Date = 06-09-2024
+
+# is zero an integer?
+null = 0
+print(type(null))
+# yes
+
+# Ways of naming variables?
+# Two main conventions:
+# Camelcase: firstName
+# Underscores: first_name
+# Can we name variables the same as functions?
+# print= "will this work"
+# print(print) gives TypeError
+Print="what about this"
+print(Print)
+# first name = "okay, try this" SyntaxError
+# first.name = "...this one?" SyntaxError
+# first%name = "%%%" SyntaxError
+# first&name = "&&&" SyntaxError
+first2name="numbers?"
+print(first2name)
+# 2 = "lone numbers?" SyntaxError
+# _firstname = "underscore beginning"
+# print(_firstname) TypeError
+_="underscore"
+print(_)
+
+print("\n\n")
+
+# Arithmetic Operations
+x = 7
+y = 3
+ print("Today's magic number:", x+y)
