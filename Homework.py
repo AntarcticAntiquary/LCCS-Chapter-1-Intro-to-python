c9c5bafd90e3e5655f0c7e6f4cd55cbb63187218
AntarcticAntiquary <ctw20.daniel.lewis@corketb.ie>
2024-09-15 21:15:52 +0100

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,54 @@
+"""
+Author='Daniel Lewis'
+Telos=Homework
+Identifier='ATM Tasks'
+Date=15-09-2024
+(My computer at home is a Mac and doesn't have hashtags)
+"""
+
+print("\t ________________________________ ")
+print("\t|\t\t\t\t |")
+print("\t|\t LCCS BANK LIMITED\t |")
+print("\t|\t ATM Main Menu:\t\t |")
+print("\t|\t\t\t\t |")
+print("\t|\t1. Balance Enquiry\t |")
+print("\t|\t2. Cash Lodgement\t |")
+print("\t|\t3. Cash Withdrawal\t |")
+print("\t|\t4. Cash Transfer\t |")
+print("\t|\t5. Bill Payment\t\t |")
+print("\t|\t6. Change PIN\t\t |")
+print("\t|\t7. Other Services\t |")
+print("\t|\t8. Close\t\t |")
+print("\t|________________________________|")
+print("\t|\t\t\t\t |")
+print("\t| CHOOSE AN OPTION >> \t\t |")
+print("\t|\t\t\t\t |")
+print("\t|________________________________|")
+print("\n\n")
+
+'''
+GUIs (Graphical User Interface) use images, rather than text, to present menus
+They therefore have a much smaller learning curve
+However they are a lot more difficult to code
+
+A program with end-users unused to computer science should probably use a GUI
+A smaller program with tech-savvy users can use a console menu
+Or possibly, there are not enough resources to make a GUI
+'''
+
+print("\t _______________________________ ")
+print("\t| \t\t\t\t|")
+print("\t| \tCIVILISATION X\t\t|")
+print("\t| \tMAIN MENU:\t\t|")
+print("\t| \t\t\t\t|")
+print("\t| \tSINGLEPLAYER\t\t|")
+print("\t| \tMULTIPLAYER\t\t|")
+print("\t| \tHOTSEAT\t\t\t|")
+print("\t| \tSCENARIOS\t\t|")
+print("\t| \tCIVILOPEDIA\t\t|")
+print("\t| \tPROFILE\t\t\t|")
+print("\t| \tACHIEVEMENTS\t\t|")
+print("\t| \tCREDITS\t\t\t|")
+print("\t| \t\t\t\t|")
+print("\t| \tEXIT GAME\t\t|")
+print("\t|_______________________________|")
