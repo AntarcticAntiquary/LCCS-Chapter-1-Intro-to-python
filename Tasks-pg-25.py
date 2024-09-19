c7d14815e052f45072056c58f9bed2cd27e66f71
AntarcticAntiquary <ctw20.daniel.lewis@corketb.ie>
2024-09-19 22:14:22 +0100

    Add files via upload

---
+++ Tasks-pg-25.py
@@ -0,0 +1,12 @@
+"""
+Author='Daniel Lewis'
+Telos=Homework
+Topic='Strings, Inputs and ASCII'
+Identifier='Page 25, Task 1'
+Date=19-09-2024
+"""
+
+print('Task-1: \n')
+greatMusician=input('What is your favourite music artist? :')
+descriptor='talented'
+print(greatMusician+' is very '+descriptor+'!')
