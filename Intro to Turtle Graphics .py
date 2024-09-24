3e8d1b13a58763bb43d39bf06e9d25a583248347
AntarcticAntiquary <ctw20.daniel.lewis@corketb.ie>
2024-09-24 22:02:58 +0100

    Update Intro to Turtle Graphics .py

--- Intro to Turtle Graphics .py
+++ Intro to Turtle Graphics .py
@@ -21,3 +21,10 @@ for pattern in range(0,6):
         turtle.forward(200)
         turtle.right(144)
     turtle.right(6)
+
+# Task 7
+import random
+for random in range(random.randint(1,10),random.randint(10,60)):
+    turtle.pencolor(random.choice(['red','blue','green','yellow'])
+    turtle.forward(random.randint(10,100))
+    turtle.right(random.randint(0,360))
